eccf53e5aab72c7f3d01b5bf159f22697ec21940
Valéry Farcy <valery.farcy@esiea.fr>
2025-11-05 12:31:17 +0100

    tests fixed

--- calculs.test.js
+++ calculs.test.js
@@ -26,3 +26,47 @@ describe('calculerImpot', () => {
         expect(calculerImpot(1000000)).toBeCloseTo(252678);
     });
 });
+
+describe('Options du survivant - exemples pédagogiques', () => {
+    const communaute = 200000;
+    const propreM = 100000;
+    const propreMme = 50000;
+    const donationE = 20000;
+    const abattement = 100000 - donationE; // 80 000
+    const ageSurvivant = 75;
+    const valeurUsufruit = getValeurUsufruit(ageSurvivant); // 0.30
+    const valeurNP = 1 - valeurUsufruit; // 0.70
+    // Patrimoine de Monsieur
+    const patrimoineM = (communaute / 2) + propreM; // 200 000
+
+    test('100% Usufruit', () => {
+        const partTaxableEnfant = (patrimoineM * valeurNP) / 2; // 70 000
+        const baseTaxable = Math.max(0, partTaxableEnfant - abattement); // 0
+        expect(baseTaxable).toBe(0);
+        expect(calculerImpot(baseTaxable)).toBe(0);
+    });
+
+    test('1/4 Pleine Propriété', () => {
+        const partEnfants = patrimoineM * 0.75; // 150 000
+        const partTaxableEnfant = partEnfants / 2; // 75 000
+        const baseTaxable = Math.max(0, partTaxableEnfant - abattement); // 0
+        expect(baseTaxable).toBe(0);
+        expect(calculerImpot(baseTaxable)).toBe(0);
+    });
+
+    test('1/3 Pleine Propriété', () => {
+        const partEnfants = patrimoineM * (2/3); // 133333.33
+        const partTaxableEnfant = partEnfants / 2; // 66666.67
+        const baseTaxable = Math.max(0, partTaxableEnfant - abattement); // 0
+        expect(Math.round(baseTaxable)).toBe(0);
+        expect(calculerImpot(baseTaxable)).toBe(0);
+    });
+
+    test('1/4 PP + 3/4 Usufruit', () => {
+        const partUSU = patrimoineM * 0.75; // 150 000
+        const partTaxableEnfant = (partUSU * valeurNP) / 2; // 52 500
+        const baseTaxable = Math.max(0, partTaxableEnfant - abattement); // 0
+        expect(baseTaxable).toBe(0);
+        expect(calculerImpot(baseTaxable)).toBe(0);
+    });
+});
